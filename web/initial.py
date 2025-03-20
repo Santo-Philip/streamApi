feefd85647b59dbf777136effa5d2ba8161957de
Santo-Philip <santop7510@gmail.com>
2025-03-20 22:35:18 +0530

    added server stats

--- web/initial.py
+++ web/initial.py
@@ -4,6 +4,7 @@ import logging
 
 from web.index import video_index, delete_video
 from web.protected_page import auth_middleware
+from web.server import websocket_handler, index_handler
 
 logger = logging.getLogger(__name__)
 async def start_web_server():
@@ -13,6 +14,8 @@ async def start_web_server():
     app.router.add_get('/video/{token}', serve_video_player)
     app.router.add_get('/videos', video_index)
     app.router.add_delete('/videos/{token}', delete_video)
+    app.router.add_get('/server-stats', websocket_handler)
+    app.router.add_get('/server', index_handler)
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, "0.0.0.0", 8080)
---
+++ web/server.py
@@ -0,0 +1,38 @@
+import aiohttp
+from aiohttp import web
+import psutil
+import asyncio
+import json
+
+async def get_server_stats():
+    # Collect server stats
+    cpu = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+    net = psutil.net_io_counters()
+    bandwidth_sent = net.bytes_sent / 1024 / 1024  # MB
+    bandwidth_recv = net.bytes_recv / 1024 / 1024  # MB
+    return {
+        'cpu': cpu,
+        'ram': ram,
+        'disk': disk,
+        'bandwidth_sent': bandwidth_sent,
+        'bandwidth_recv': bandwidth_recv
+    }
+
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    # Send stats every 2 seconds
+    while not ws.closed:
+        stats = await get_server_stats()
+        await ws.send_json(stats)
+        await asyncio.sleep(2)
+
+    return ws
+
+async def index_handler(request):
+    # Serve the HTML page with the graph
+    with open('server.html', 'r') as f:
+        return web.Response(text=f.read(), content_type='text/html')
